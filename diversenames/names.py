56396d270e49f282b9cf899bfa169c360e49f3be
Nikhil <nikhilbehari@gmail.com>
2020-05-08 14:29:15 -0400

    Correct txt file path issue

--- diversenames/names.py
+++ diversenames/names.py
@@ -1,10 +1,10 @@
 import random
 import linecache
+import os
 
 
 def generate_name(n=1):
-    file = "names.txt"
+    folder_path = os.path.dirname(os.path.abspath(__file__))
+    file = os.path.join(folder_path, 'names.txt')
     sample_lines = random.sample(range(19948), n)
     return [linecache.getline(file, i) for i in sample_lines]
-
-print(generate_name(4))
